4c0737a569539068c9bec3d783b20a37300e9ff3
shubham <shubhama3ps@gmail.com>
2019-08-01 01:19:37 +0530

    now working

--- model/network.py
+++ model/network.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 import numpy as np
 
 
-def initialize():
-    random_normal = tf.initializers.RandomNormal()
+def initialize(num_features, num_classes):
+
     n_h1 = 50
     n_h2 = 25
 
+    print("hey: ", num_features, num_classes)
     parameters = {
 
         'h_1': tf.Variable(random_normal([num_features, n_h1]), name='h_1', dtype='float32'),
@@ -23,7 +24,7 @@ def initialize():
 
 
 def neural_net(x, parameters):
-    layer_1 = tf.add(tf.matmul(x, parameters['h_1']), parameters['b1'])
+    layer_1 = tf.add(tf.matmul([x], parameters['h_1']), parameters['b1'])
     layer_1 = tf.nn.sigmoid(layer_1)
 
     layer_2 = tf.add(tf.matmul(layer_1, parameters['h_2']), parameters['b2'])
@@ -35,15 +36,22 @@ def neural_net(x, parameters):
     return tf.nn.softmax(out_layer)
 
 
-def cross_entropy(y_true, y_pred):
-    y_true = tf.one_hot(y_true, num_classes)
-    y_pred = tf.clip_by_value(y_pred, 1e-9,1.)
+def cross_entropy(y_true, y_pred, n_classes):
+    y_true = tf.one_hot(y_true, n_classes)
+    y_pred = tf.clip_by_value(y_pred, 1e-9, 1.)
 
     return tf.reduce_mean(-tf.reduce_sum(y_true * tf.math.log(y_pred)))
 
 
 def accuracy(y_pred, y_true):
-    correct_predictions = tf.equal(tf.argmax(y_pred,1), tf.cast(y_true, tf.int64))
+    print("accc: ", y_pred.shape)
+    # y_true = tf.reshape(y_true, num_classes, [225, 1])
+    y_pred = tf.reshape(y_pred, [-1, 2])
+    # print("accc1: ", y_true.shape)
+    # print('req: ', tf.argmax(y_pred, 0).shape)
+    # print("example: \n::", y_true[2:5],"\n \n" , y_pred[2:5, :], tf.argmax(y_pred, 1))
+    correct_predictions = tf.equal(tf.argmax(y_pred, 1), tf.cast(y_true, tf.int64))
+    # print("c_p: ", correct_predictions)
     return tf.reduce_mean(tf.cast(correct_predictions, tf.float32), axis=-1)
 
 
@@ -53,17 +61,18 @@ def run_optimization(x, y, parameters, optimizer):
         # print("1")
         # print("a: ", g.watched_variable())
         pred = neural_net(x, parameters)
-        loss = cross_entropy(y_true=y, y_pred=pred)
+        loss = cross_entropy(y_true=y, y_pred=pred, n_classes=num_classes)
         # print("b: ", g.watched_variables())
 
 
     # print(weights.values())
     trainable_variables = [parameters['h_1'], parameters['b1'], parameters['h_2'], parameters['b2'],
                            parameters['h_out'], parameters['b_out']]
-    gradients = g.gradient(loss, trainable_variables)
+    gradients = g.gradient(loss, list(parameters.values()))
     # print("b4: ", parameters['h_1'].shape)
     # print("now: ", gradients[0].shape)
-    optimizer.apply_gradients(zip(gradients, trainable_variables))
+    # print(type(parameters['h_1']), type(gradients))
+    optimizer.apply_gradients(zip(gradients, list(parameters.values())))
     # print("c: ", dir(g))
     # print("a4: ", parameters['h_1'].shape)#, parameters['b1'], parameters['h_2'], parameters['b2'], parameters['h_out'], parameters['b_out'])
 
@@ -77,29 +86,44 @@ if __name__ == '__main__':
     test_img_path = '/Users/shubham/Documents/projects/data/flowerRecognition/test'
     test_class_path = '/Users/shubham/Documents/projects/data/flowerRecognition/test.csv'
     #
-    train_data, train_label = img_utils.load_data(train_img_path, train_class_path)
+    train_data, train_label, test_data, test_label = img_utils.load_data(train_img_path, train_class_path)
     # test_data, test_label = img_utils.load_data(test_img_path, test_class_path)
 
     num_features = 64 * 64 * 3
     n_h1 = 50
     n_h2 = 25
-    num_classes = 102
+    num_classes = 2
 
-    display_step = 100
+    display_step = 300
 
-    optimizer = tf.optimizers.SGD(learning_rate=0.01)
+    random_normal = tf.initializers.RandomNormal()
+
+    optimizer = tf.optimizers.SGD(learning_rate=0.0001)
     train_data = tf.convert_to_tensor(train_data, dtype='float32')
+    test_data = tf.convert_to_tensor(test_data, dtype='float32')
 
-    for i in range(1000):
-        parameters = initialize()
+    parameters = initialize(num_features, num_classes)
+
+    for i in range(30000):
+
         run_optimization(train_data, train_label, parameters, optimizer)
 
         if i % display_step == 0:
-            print("hey")
             pred = neural_net(train_data, parameters)
-            loss = cross_entropy(train_label, pred)
+            loss = cross_entropy(train_label, pred, n_classes=num_classes)
+
             acc = accuracy(pred, train_label)
+            print("loss: ", loss, acc)
             print(("step: %i, loss: %f, accuracy: %f" % (i, loss, acc)))
+
+            # test_data, test_label = img_utils.load_data(test_img_path, test_class_path)
+            print(train_data.shape, test_data.shape)
+            pred = neural_net(test_data, parameters)
+            loss = cross_entropy(test_label, pred, n_classes=num_classes)
+            acc = accuracy(pred, test_label)
+            print(("step: %i, loss: %f, accuracy: %f" % (i, loss, acc)))
+
+
 
 
 
--- preprocessing/img_utils.py
+++ preprocessing/img_utils.py
@@ -2,6 +2,7 @@ import cv2
 import glob
 import pandas as pd
 import numpy as np
+import os
 
 
 def load_data(img_path, label_path):
@@ -10,28 +11,103 @@ def load_data(img_path, label_path):
     label_df = pd.read_csv(label_path)
 
     label_df = label_df.sort_values('image_id', axis=0)
-    assert label_df['image_id'].count() == len(files)
+    # assert label_df['image_id'].count() == len(files)
 
     x = []
-    y = label_df['category'].values.reshape(-1)
+    y = []
+    # y = label_df['category'].values.reshape(-1)
+
+
+
+
     for file in files:
-        img = cv2.imread(file)
-        img = cv2.resize(img, (64, 64)).reshape(-1)
-        # img = img/255.0
-        x.append(img)
+
+        file_label = label_df[label_df['image_id'] == int(os.path.basename(file).rstrip('.jpg'))]
+        label = file_label['category'].values[0]
+
+        if label == 2 or label == 1:
+            img = cv2.imread(file)
+            img = cv2.resize(img, (64, 64)).reshape(-1)
+            # img = img/255.0
+            x.append(img)
+            if label == 2:
+                y.append(1)
+            else:
+                y.append(0)
 
     x = np.asarray(x)
+    y = np.asarray(y)
     x = x/255.
     print(x.shape, y.shape)
 
     assert x.shape[0] == y.shape[0]
 
-    return x, y
+    return x[:170, :], y[:170], x[170:, :], y[170:]
 
-
-if __name__ == '__main__':
-    img_path = '/Users/shubham/Documents/projects/data/flowerRecognition/train'
-    class_path = '/Users/shubham/Documents/projects/data/flowerRecognition/train.csv'
-
-    load_data(img_path, class_path)
-
+#
+# if __name__ == '__main__':
+#     img_path = '/Users/shubham/Documents/projects/data/flowerRecognition/train'
+#     class_path = '/Users/shubham/Documents/projects/data/flowerRecognition/train.csv'
+#
+#     load_data(img_path, class_path)
+#
+#
+# import cv2
+# import glob
+# import pandas as pd
+# import numpy as np
+# import os
+#
+#
+# def load_data(img_path, label_path):
+#
+#     files = glob.glob(img_path+'/*.jpg')
+#     files.sort()
+#
+#
+#
+#     label_df = pd.read_csv(label_path)
+#     label_df = label_df.sort_values('image_id', axis=0)
+#
+#     file_label = label_df[label_df['image_id'] == int(os.path.basename(file).rstrip('.jpg'))]
+#     label = file_label['category'].values[0]
+#
+#     if label == 2 or label == 1:
+#
+#     # assert label_df['image_id'].count() == len(files)
+#
+#
+#     x = []
+#     y = []
+#
+#     for file in files:
+#
+#         file_label = label_df[label_df['image_id']==int(os.path.basename(file).rstrip('.jpg'))]
+#         label = file_label['category'].values[0]
+#
+#         if label == 2 or label == 1:
+#             # print(label)
+#             img = cv2.imread(file)
+#             img = cv2.resize(img, (64, 64)).reshape(-1)
+#
+#             y.append(label)
+#             x.append(img)
+#
+#     x = np.asarray(x)
+#     y = np.asarray(y)
+#     x = x/255.
+#     x = x.reshape(12288,-1)
+#     y = y.reshape(-1)
+#     print(x.shape, y.shape)
+#     print(set(y))
+#
+#     # assert x.shape[0] == y.shape[0]
+#
+#     return x, y
+#
+#
+# if __name__ == '__main__':
+#     img_path = '/Users/shubham/Documents/projects/data/flowerRecognition/train'
+#     class_path = '/Users/shubham/Documents/projects/data/flowerRecognition/train.csv'
+#
+#     load_data(img_path, class_path)
